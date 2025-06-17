6f5250e3db61fa4af30a7157f4140e967c240ee0
Raihan260 <rayhanandika26@gmail.com>
2025-06-17 17:09:16 +0700

    Viucio

--- app/src/main/java/com/example/mcsexlec/ProfileActivity.kt
+++ app/src/main/java/com/example/mcsexlec/ProfileActivity.kt
@@ -1,20 +1,51 @@
 package com.example.mcsexlec
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.PopupMenu
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var kebabMenu: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_profile)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Inisialisasi tombol kebab menu
+        kebabMenu = findViewById(R.id.kebabMenu)
+
+        // Set klik listener untuk menampilkan popup menu
+        kebabMenu.setOnClickListener { view ->
+            showPopupMenu(view)
         }
+    }
+
+    private fun showPopupMenu(view: View) {
+        val popup = PopupMenu(this, view)
+        popup.menuInflater.inflate(R.menu.menu_popup, popup.menu)
+
+        popup.setOnMenuItemClickListener { menuItem: MenuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_home -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    true
+                }
+                R.id.menu_read_later -> {
+                    startActivity(Intent(this, ReadLaterActivity::class.java))
+                    true
+                }
+                R.id.menu_profile -> true
+                else -> false
+            }
+        }
+
+        popup.show()
     }
 }
